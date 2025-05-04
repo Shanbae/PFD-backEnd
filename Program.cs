3f0f1e24b04c966b83763cbeb7d482e60d47f805
Shanbae <ampssarathi7799@gmail.com>
2025-05-04 08:12:18 +0530

    cors changeshas been done again

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("https://personalfinance-opal.vercel.app").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+            policy.WithOrigins("https://personalfinance-opal.vercel.app").AllowAnyMethod().AllowAnyHeader()/*.AllowCredentials()*/;
         });
 
 
